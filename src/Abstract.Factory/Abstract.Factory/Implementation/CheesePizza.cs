790f0cf2faeb9d719c3d3248fc8fec4b76dffe6a
Jordan Lee <jordan@jordanlee.net>
2021-12-14 15:56:25 +1100

    Add example of Abstract Factory

---
+++ src/Abstract.Factory/Abstract.Factory/Implementation/CheesePizza.cs
@@ -0,0 +1,22 @@
+﻿using Abstract.Factory.Pattern;
+
+namespace Abstract.Factory.Implementation
+{
+    public class CheesePizza : BasePizza
+    {
+        public CheesePizza(AbstractIngredientFactory ingredientFactory) : base(ingredientFactory)
+        {
+        }
+
+        public override void Bake()
+        {
+            _cheese = _ingredientFactory.CreateCheese();
+            _dough = _ingredientFactory.CreateDough();
+
+            Console.WriteLine($"Baked a {this.GetType().Name} with the following ingredients:");
+            Console.WriteLine(_cheese);
+            Console.WriteLine(_dough);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/Abstract.Factory/Abstract.Factory/Pattern/BasePizza.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstract.Factory.Pattern
+{
+    public abstract  class BasePizza
+    {
+        internal string _dough;
+        internal string _cheese;
+        internal AbstractIngredientFactory _ingredientFactory;
+
+        public BasePizza(AbstractIngredientFactory ingredientFactory)
+        {
+            _ingredientFactory = ingredientFactory;
+        }
+
+        public abstract void Bake();
+    }
+}
