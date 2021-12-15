fd9efc913e46e40058a3ba90728d7aabc3663afc
Jordan Lee <jordan@jordanlee.net>
2021-12-15 14:07:07 +1100

    Add Singleton pattern example

---
+++ src/Singleton/Singleton/Pattern/ClassicSingleton.cs
@@ -0,0 +1,27 @@
+﻿namespace Singleton.Pattern
+{
+    public class ClassicSingleton
+    {
+        private static ClassicSingleton _classicSingleton;
+        private static string _singletonValue;
+
+        private ClassicSingleton()
+        {
+            _singletonValue = Guid.NewGuid().ToString(); // Guid is also itself follows the Singleton pattern.
+        }
+
+
+        public static ClassicSingleton GetInstance()
+        {
+            if (_classicSingleton == null)
+                _classicSingleton = new ClassicSingleton();
+                
+            return _classicSingleton;
+        }
+
+        public void PrintValue()
+        {
+            Console.WriteLine($"Value: {_singletonValue}");
+        }
+    }
+}
