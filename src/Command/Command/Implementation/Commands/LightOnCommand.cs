38ee00bfd4579f7ba2ba51cbb251c5858c111551
Jordan Lee <jordan@jordanlee.net>
2021-12-16 23:54:15 +1100

    Add Command Pattern example (#10)

---
+++ src/Command/Command/Implementation/Commands/LightOnCommand.cs
@@ -0,0 +1,19 @@
+﻿using Command.Implementation.Receivers;
+using Command.Pattern;
+
+namespace Command.Implementation.Commands
+{
+    public class LightOnCommand : ICommand
+    {
+        private Light _light;
+
+        public LightOnCommand(Light light)
+        {
+            _light = light;
+        }
+        public void Execute()
+        {
+            _light.On();
+        }
+    }
+}
---
+++ src/Command/Command/Pattern/IInvoker.cs
@@ -0,0 +1,7 @@
+﻿namespace Command.Pattern
+{
+    public interface IInvoker
+    {
+        void SetCommand(int commandSlot, ICommand onCommand, ICommand offCommand);
+    }
+}
